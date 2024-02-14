e96dd18111b9ea2a4a1139bdd9307b38ad002f55
yuta-nishi <ypmtk437.0209@gmail.com>
2024-02-14 22:25:45 +0900

    feat: add checking conflict of shortcut

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,15 +1,40 @@
+import type { Message } from '~/types/message';
+
+/**
+ * Extension install process
+ */
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    checkCommandShortcuts();
+  }
+});
+
+/**
+ * Icon click process
+ */
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const activeTab = tabs[0];
-    if (!activeTab) {
-      return;
-    }
-    const url = activeTab.url;
+    const url = activeTab?.url || '';
+    const id = activeTab?.id || 0;
+    const message: Message = { type: 'copy', text: url! };
     (async () => {
-      if (!activeTab.id) {
-        return;
-      }
-      chrome.tabs.sendMessage(activeTab.id, { type: 'copy', text: url });
+      chrome.tabs.sendMessage(id, message);
     })();
   });
 });
+
+/**
+ * Check if any command shortcuts are conflicting
+ */
+const checkCommandShortcuts = () => {
+  chrome.commands.getAll((commands) => {
+    const missingShortcuts = commands
+      .filter(({ shortcut }) => shortcut === '')
+      .map(({ name }) => name);
+
+    if (missingShortcuts.length > 0) {
+      chrome.runtime.openOptionsPage();
+    }
+  });
+};
