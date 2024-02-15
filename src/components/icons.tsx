6ccf2ba133bd9122fbcbf4f2ce529141e70c1310
yuta-nishi <ypmtk437.0209@gmail.com>
2024-02-16 00:43:05 +0900

    feat: add alert component

---
+++ src/components/icons.tsx
@@ -0,0 +1,7 @@
+import { AlertCircle, AlertOctagon, AlertTriangle } from 'lucide-react';
+
+export const Icons = {
+  alert_circle: AlertCircle,
+  alert_octagon: AlertOctagon,
+  alert_triangle: AlertTriangle,
+};
