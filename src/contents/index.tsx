b4a9f836ba7c172df88925390a64dfb91dc4b5af
yuta-nishi <ypmtk437.0209@gmail.com>
2024-02-15 22:59:12 +0900

    feat: add toast notification for successful copy

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -1,22 +1,64 @@
-import type { PlasmoCSConfig } from 'plasmo';
+import type { PlasmoCSConfig, PlasmoGetStyle } from 'plasmo';
+import { useEffect, useState } from 'react';
+
+// Add tailwind css for shadcn/ui components
+// Without this, shadcn/ui components will not be displayed correctly
+import '~/style.css';
+
+import cssText from 'data-text:~/style.css';
+import { Toaster } from '~/components/ui/toaster';
+import { useToast } from '~/components/ui/use-toast';
+import type { Message } from '~/types/message';
 
 export const config: PlasmoCSConfig = {
   matches: ['<all_urls>'],
 };
 
-navigator.permissions.query({ name: 'clipboard-write' as PermissionName }).then((res) => {
-  if (res.state === 'granted' || res.state === 'prompt') {
-    chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
-      if (message.type === 'copy') {
-        navigator.clipboard.writeText(message.text).then(
-          () => {
-            console.log('copied');
-          },
-          () => {
-            console.error('failed to copy');
+export const getStyle: PlasmoGetStyle = () => {
+  const style = document.createElement('style');
+  style.textContent = cssText;
+  return style;
+};
+
+const Contents = (): JSX.Element => {
+  const [notification, setNotification] = useState<string>('');
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (notification) {
+      toast({
+        description: notification,
+      });
+      setNotification('');
+    }
+  }, [notification]);
+
+  navigator.permissions
+    .query({ name: 'clipboard-write' as PermissionName })
+    .then((res) => {
+      if (res.state === 'granted' || res.state === 'prompt') {
+        chrome.runtime.onMessage.addListener(
+          (message: Message, _sender, _sendResponse) => {
+            if (message.type === 'copy') {
+              navigator.clipboard.writeText(message.text).then(
+                () => {
+                  setNotification('Copied Current URL');
+                },
+                () => {
+                  setNotification('Failed to copy');
+                },
+              );
+            }
           },
         );
       }
     });
-  }
-});
+
+  return (
+    <>
+      <Toaster />
+    </>
+  );
+};
+
+export default Contents;
