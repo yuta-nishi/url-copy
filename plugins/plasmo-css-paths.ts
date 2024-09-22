fd57597fcac4013ed42800fff19528fd656ebb8b
yuta-nishi <ypmtk437.0209@gmail.com>
2024-09-23 01:44:21 +0900

    chore: Change plugin directory

--- plugins/plasmo-css-paths.ts
+++ plugins/plasmo-css-paths.ts
@@ -1,19 +1,23 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import type { Plugin } from 'vite';
 
-import type { PlasmoCssPlugin } from '~/types/plugin';
+type PlasmoCssPlugin = Plugin & {
+  resolveId: (source: string) => string | null;
+  load: (id: string) => string | null;
+};
 
 const plasmoCssPaths = (): PlasmoCssPlugin => {
   return {
     name: 'plasmo-css-paths',
     enforce: 'pre',
-    resolveId: (source) => {
+    resolveId: (source: string) => {
       if (source.startsWith('data-text:~')) {
         return source;
       }
       return null;
     },
-    load: (id) => {
+    load: (id: string) => {
       if (id.startsWith('data-text:~')) {
         const realPath = id.slice('data-text:~'.length);
         return fs.readFileSync(path.join(process.cwd(), 'src', realPath), 'utf-8');
--- src/types/plugin.ts
+++
@@ -1,6 +0,0 @@
-import type { Plugin } from 'vite';
-
-export interface PlasmoCssPlugin extends Plugin {
-  resolveId: (source: string) => string | null;
-  load: (id: string) => string | null;
-}
